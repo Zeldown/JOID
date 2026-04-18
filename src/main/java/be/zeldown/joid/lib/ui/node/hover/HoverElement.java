ebba383cf2327cd6694286dc9747c269d6ad0be8
Zeldown <dev.zeldown@gmail.com>
2026-04-18 12:09:49 +0400

    fix: CustomHoverElement & NodeHoverElement (🐛)

--- src/main/java/be/zeldown/joid/lib/ui/node/hover/HoverElement.java
+++ src/main/java/be/zeldown/joid/lib/ui/node/hover/HoverElement.java
@@ -8,4 +8,9 @@ public interface HoverElement {
 
 	public void render(final @NonNull Node node, final double mouseX, final double mouseY);
 
+	default double getX() { return 0; }
+	default double getY() { return 0; }
+	default double getWidth() { return 0; }
+	default double getHeight() { return 0; }
+
 }
