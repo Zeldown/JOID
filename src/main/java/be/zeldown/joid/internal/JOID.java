3c67d53812ecc56fd623d376ee598c8055059607
Zeldown <dev.zeldown@gmail.com>
2026-04-28 23:42:18 +0400

    update: bump to 6.2.0 (🔖)

--- src/main/java/be/zeldown/joid/internal/JOID.java
+++ src/main/java/be/zeldown/joid/internal/JOID.java
@@ -16,7 +16,7 @@ import lombok.NonNull;
 @Getter
 public final class JOID {
 
-	public static final String VERSION = "6.1.2";
+	public static final String VERSION = "6.2.0";
 
 	private static JOID instance;
 
